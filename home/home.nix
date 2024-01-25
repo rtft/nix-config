4c38bc39febde05cb261e13c60f5bcb8e867b99d
rftf <u1025269@utah.edu>
2024-01-24 17:22:06 -0800

    removed awesome references

--- home/home.nix
+++ home/home.nix
@@ -14,7 +14,7 @@
 
     # You can also split up your configuration and import pieces of it here:
     # ./nvim.nix
-    ../modules/desktop/awesome.nix
+    # ../modules/desktop/awesome.nix
 #    ../modules/shared/cli.nix
 
     
--- modules/desktop/awesome.nix
+++ modules/desktop/awesome.nix
@@ -1,10 +1,10 @@
-{ inputs, config, pkgs, lib, ... }:
-{
+# { inputs, config, pkgs, lib, ... }:
+# {
 
-  home.file = {
-    ".config/awesome".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-config/config/awesome";
-    "nix-config/config/awesome/modules/bling".source = inputs.bling.outPath;
-    "nix-config/config/awesome/modules/rubato".source = inputs.rubato.outPath;
-  };
+#   home.file = {
+#     ".config/awesome".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix-config/config/awesome";
+#     "nix-config/config/awesome/modules/bling".source = inputs.bling.outPath;
+#     "nix-config/config/awesome/modules/rubato".source = inputs.rubato.outPath;
+#   };
 
-}
+# }
