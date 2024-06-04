dde79e5a380867fbe775a8a5fcddfdf3db9fd3c5
rftf <u1025269@utah.edu>
2024-06-03 22:39:30 -0700

    Update to 24.05

--- flake.nix
+++ flake.nix
@@ -3,17 +3,16 @@
 
   inputs = {
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # Home manager
-    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     hyprland.url = "github:hyprwm/Hyprland";
 
     # TODO: Add any other flake you might need
-    # hardware.url = "github:nixos/nixos-hardware";
 
     # nixpkgs-f2k.url = "github:fortuneteller2k/nixpkgs-f2k";
 
@@ -22,12 +21,12 @@
   };
 
   outputs = {
-    self,
+    self, # TODO: probably don't need this
     nixpkgs,
     home-manager,
     ...
   } @ inputs: let
-    inherit (self) outputs;
+    inherit (self) outputs; # TODO: probably don't need this
   in {
     # NixOS configuration entrypoint
     # Available through 'nixos-rebuild --flake .#your-hostname'
