a4dc52ccb8535e41f0b55cbfc14f97edb0d5d624
rftf <u1025269@utah.edu>
2024-06-26 21:01:58 -0700

    format changes

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -27,58 +27,58 @@
 
       bind = [
       	    # Applications
-            "$mod, return, exec, kitty"
-            "$mod, d, exec, rofi -show drun"
-            "$mod, w, exec, rofi -show window"
-            "$mod, r, exec, rofi -show run"
-            "$mod, s, exec, rofi -show ssh"
-            "$mod, p, exec, rofi -show power"
-	    "$mod, C, killactive"
-	    "$mod, V, togglefloating"
+      	"$mod, return, exec, kitty"
+      	"$mod, d, exec, rofi -show drun"
+      	"$mod, w, exec, rofi -show window"
+      	"$mod, r, exec, rofi -show run"
+      	"$mod, s, exec, rofi -show ssh"
+      	"$mod, p, exec, rofi -show power"
+	    	"$mod, C, killactive"
+	    	"$mod, V, togglefloating"
 
-	    # Movement 
-	    "$mod, left, movefocus, l"
-	    "$mod, right, movefocus, r"
-	    "$mod, up, movefocus, u"
-	    "$mod, down, movefocus, d"
-	    "$mod, h, movefocus, l"
-	    "$mod, l, movefocus, r"
-	    "$mod, k, movefocus, k"
-	    "$mod, j, movefocus, j"
+	    	# Movement 
+	    	"$mod, left, movefocus, l"
+	    	"$mod, right, movefocus, r"
+	    	"$mod, up, movefocus, u"
+	    	"$mod, down, movefocus, d"
+	    	"$mod, h, movefocus, l"
+	    	"$mod, l, movefocus, r"
+	    	"$mod, k, movefocus, k"
+	    	"$mod, j, movefocus, j"
 
-	    # Workspaces 
-	    "$mod, 1, workspace, 1"
-	    "$mod, 2, workspace, 2"
-	    "$mod, 2, workspace, 2"
-	    "$mod, 3, workspace, 3"
-	    "$mod, 4, workspace, 4"
-	    "$mod, 5, workspace, 5"
-	    "$mod, 5, workspace, 6"
-	    "$mod, 6, workspace, 7"
-	    "$mod, 7, workspace, 8"
-	    "$mod, 9, workspace, 9"
-	    "$mod, 0, workspace, 10"
+	    	# Workspaces 
+	    	"$mod, 1, workspace, 1"
+	    	"$mod, 2, workspace, 2"
+	    	"$mod, 2, workspace, 2"
+	    	"$mod, 3, workspace, 3"
+	    	"$mod, 4, workspace, 4"
+	    	"$mod, 5, workspace, 5"
+	    	"$mod, 5, workspace, 6"
+	    	"$mod, 6, workspace, 7"
+	    	"$mod, 7, workspace, 8"
+	    	"$mod, 9, workspace, 9"
+	    	"$mod, 0, workspace, 10"
 
-	    # Workspace move
-	    "$mod SHIFT, 1, movetoworkspace, 1"
-	    "$mod SHIFT, 2, movetoworkspace, 2"
-	    "$mod SHIFT, 3, movetoworkspace, 3"
-	    "$mod SHIFT, 4, movetoworkspace, 4"
-	    "$mod SHIFT, 5, movetoworkspace, 5"
-	    "$mod SHIFT, 6, movetoworkspace, 6"
-	    "$mod SHIFT, 7, movetoworkspace, 7"
-	    "$mod SHIFT, 8, movetoworkspace, 8"
-	    "$mod SHIFT, 9, movetoworkspace, 9"
-	    "$mod SHIFT, 10, movetoworkspace, 10"
+	    	# Workspace move
+	    	"$mod SHIFT, 1, movetoworkspace, 1"
+	    	"$mod SHIFT, 2, movetoworkspace, 2"
+	    	"$mod SHIFT, 3, movetoworkspace, 3"
+	    	"$mod SHIFT, 4, movetoworkspace, 4"
+	    	"$mod SHIFT, 5, movetoworkspace, 5"
+	    	"$mod SHIFT, 6, movetoworkspace, 6"
+	    	"$mod SHIFT, 7, movetoworkspace, 7"
+	    	"$mod SHIFT, 8, movetoworkspace, 8"
+	    	"$mod SHIFT, 9, movetoworkspace, 9"
+	    	"$mod SHIFT, 10, movetoworkspace, 10"
 
-	    # Scroll through existing workspaces
-	    "$mod, mouse_down, workspace, e+1"
-	    "$mod, mouse_up, workspace, e-1"
+	    	# Scroll through existing workspaces
+	    	"$mod, mouse_down, workspace, e+1"
+	    	"$mod, mouse_up, workspace, e-1"
       ];
 
       bindm = [
-	   "$mod, mouse:272, movewindow"
-	   "$mod, mouse:273, resizewindow"
+	   		"$mod, mouse:272, movewindow"
+	   		"$mod, mouse:273, resizewindow"
       ];
 
     }; 
