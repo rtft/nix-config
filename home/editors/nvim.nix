f9e99cf3e0a4cacf882a9f0867a4ac6bca8177c9
rtft <>
2024-07-01 19:19:00 -0700

    broken nixvim, package not working

--- home/editors/nvim.nix
+++ home/editors/nvim.nix
@@ -1,8 +1,12 @@
-{config, lib, pkgs, ...}:
+{config, lib, pkgs, inputs, ...}:
 {
+  imports = [
+   inputs.nixvim.homeManagerModules.nixvim 
+  ];
   programs.nixvim = {
     enable = true;
     plugins = {
+      bufferline.enable = true;
 			
     };
     extraPlugins = with pkgs.vimPlugins; [
